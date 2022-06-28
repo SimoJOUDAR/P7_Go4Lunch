e79ca5c2f4732126f9a43432da7479e98994aa5b
SimoJOUDAR <ssjoudar@gmail.com>
2022-06-28 13:29:21 +0200

    Empty Activities created

---
+++ app/src/main/java/fr/joudar/go4lunch/ui/activities/HomepageActivity.java
@@ -0,0 +1,16 @@
+package fr.joudar.go4lunch.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fr.joudar.go4lunch.R;
+
+public class HomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
