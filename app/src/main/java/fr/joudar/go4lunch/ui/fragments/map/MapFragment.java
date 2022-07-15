ce3bc53ca8a58e79092328b33277863a7bf30b51
SimoJOUDAR <ssjoudar@gmail.com>
2022-07-15 21:47:10 +0200

    Basic map fragment - Packages naming

--- app/src/main/java/fr/joudar/go4lunch/ui/fragments/map/MapFragment.java
+++ app/src/main/java/fr/joudar/go4lunch/ui/fragments/map/MapFragment.java
@@ -13,8 +13,7 @@ import fr.joudar.go4lunch.ui.activities.HomepageActivity;
 
 public class MapFragment extends Fragment {
 
-    public MapFragment() {
-    }
+    public MapFragment() {}
 
     public static MapFragment newInstance() {
         MapFragment fragment = new MapFragment();
@@ -27,8 +26,7 @@ public class MapFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
